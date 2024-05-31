4a599077d362495b137d01e0d3ea1003f517c25d
Luis Nascimento <lfernando.s.nascimento@gmail.com>
2024-05-31 03:17:17 -0300

    basic funcionalitty working

--- src/bin/muskp.rs
+++ src/bin/muskp.rs
@@ -5,16 +5,15 @@ use std::os::unix::net::{SocketAddr, UnixListener};
 use std::sync::atomic::AtomicBool;
 use std::thread;
 use std::{io::Write, os::unix::net::UnixStream};
-
+use glob::glob;
 use clap::{Arg, Command, Parser};
 use dialoguer::FuzzySelect;
+use std::path::Display;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let c = Command::new("muskp")
-        .subcommand(
-            Command::new("play"),
-        )
+        .subcommand(Command::new("play"))
         .subcommand(
             Command::new("search").arg(Arg::new("name").short('n').action(clap::ArgAction::Append)),
         )
@@ -24,16 +23,13 @@ async fn main() -> anyhow::Result<()> {
         .subcommand(Command::new("next"))
         .get_matches();
 
-
     let socket_path = "musicsocket";
 
     let mut socket = UnixStream::connect(socket_path).context("could not connect")?;
 
     match c.subcommand_name() {
         Some("play") => {
-            socket
-                .write(b"play")
-                .context("server did not responded")?;
+            socket.write(b"play").context("server did not responded")?;
         }
         Some("pause") => {
             socket.write(b"pause").context("server did not responded")?;
@@ -44,9 +40,9 @@ async fn main() -> anyhow::Result<()> {
         Some("list") => {
             socket.write(b"list").context("server did not responded")?;
             socket.shutdown(std::net::Shutdown::Write);
-            
+
             let mut messege = String::new();
-            
+
             socket.read_to_string(&mut messege);
 
             println!("{}", messege)
@@ -55,39 +51,28 @@ async fn main() -> anyhow::Result<()> {
             socket.write(b"next").context("server did not responded")?;
         }
         Some("search") => {
-            let query = c
-                .subcommand_matches("search")
-                .unwrap()
-                .get_one::<String>("name")
-                .ok_or("muito trint")
-                .expect("file not provided");
+            let mut musics = Vec::new();
+            for entry in glob("/home/luis/Music/**/*.mp3").expect("Failed to read glob pattern") {
+                match entry {
+                    std::result::Result::Ok(path) => musics.push(path),
+                    Err(e) => println!("{:?}", e),
+                }
+            }
+            let musics:Vec<Display> = musics.iter_mut().map(|x| {
+                x.display()
+            }).collect();
 
-            let mut message = String::new();
-            message.push_str("search");
-            message.push_str(query);
-
-            socket
-                .write(message.as_bytes())
-                .context("server did not responded")?;
-            
-            socket.shutdown(std::net::Shutdown::Write);
-            
-            let mut response = String::new();
-            socket.read_to_string(&mut response).context("maneiro");
-            
-            let list: Vec<&str> = response.split("\n").collect();
-            
             let selection = FuzzySelect::new()
                 .with_prompt("What do you choose?")
-                .items(&list)
+                .items(&musics)
                 .interact()
                 .unwrap();
 
-            println!("{}", list[selection]);
-            
+            println!("{}", musics[selection]);
+
             let mut selection_mes = String::new();
             selection_mes.push_str("path");
-            selection_mes.push_str(list[selection]);
+            selection_mes.push_str(musics[selection].to_string().as_str());
 
             socket
                 .write(selection_mes.as_bytes())
---
+++ src/bin/muskp_socket.rs
@@ -0,0 +1,122 @@
+use anyhow::{Context, Ok};
+
+use glob::glob;
+use rand::{seq::SliceRandom, thread_rng};
+use rodio::{cpal::Stream, decoder, Decoder, OutputStream, Sink, Source};
+use std::{
+    fs::File, future, io::{BufReader, Read, Write}, os::unix::net::{UnixListener, UnixStream}, path::PathBuf, sync::{Arc, BarrierWaitResult, Mutex}, thread::{self, sleep}, time::Duration
+};
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let socket_path = "/tmp/muskp";
+
+    if std::fs::metadata(socket_path).is_ok() {
+        println!("A socket is already present. Deleting...");
+        std::fs::remove_file(socket_path)
+            .with_context(|| format!("could not delete previous socket at {:?}", socket_path))?;
+    }
+
+    let mut musics = Vec::new();
+    for entry in glob("/home/luis/Music/**/*.mp3").expect("Failed to read glob pattern") {
+        match entry {
+            std::result::Result::Ok(path) => musics.push(path),
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    let mut rng = thread_rng();
+
+    let mut count: usize = 0;
+
+    musics.shuffle(&mut rng);
+
+    let musics = Arc::new(musics);
+    let unix_listener =
+        UnixListener::bind(socket_path).context("Could not create the unix socket")?;
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Arc::new(Sink::try_new(&stream_handle).unwrap());
+
+    unix_listener
+        .set_nonblocking(true)
+        .expect("could not set non blocking socket");
+
+    for stream in unix_listener.incoming() {
+        match stream {
+            std::result::Result::Ok(stream) => {
+                let sink = Arc::clone(&sink);
+                let musics = Arc::clone(&musics);
+                let t = thread::spawn(|| 
+                    handle_stream(stream)
+                );
+                let mut option = t.join().unwrap();
+                let p = thread::spawn(move || loop {
+                    let sink = Arc::clone(&sink);
+                    let musics = Arc::clone(&musics);
+                    if option == "pause" {
+                        if sink.is_paused() {
+                            sink.play()
+                        } else {
+                            sink.pause()
+                        }
+                        break;
+                    }
+                    if option == "next" {
+                        sink.skip_one();
+                        break;
+                    }
+                    if option.starts_with("path") {
+                        let path_str = option.split_off("path".len());
+                        let mut path = PathBuf::new();
+                        path.push(path_str);
+                        play_song(&sink, &path);
+                        break;
+                    }
+                    play_song(&sink, &musics[count]);
+                    count += 1
+                });
+            }
+            Err(err) => {}
+        }
+    }
+    println!("sera que chega aqui");
+    Ok(())
+}
+fn play_song(sink: &Sink, path: &PathBuf) {
+    let file = BufReader::new(File::open(path).unwrap());
+    let source = Decoder::new(file).unwrap();
+    let duration = &source.total_duration().unwrap();
+    println!("{}", path.display());
+    sink.append(source);
+
+    sleep(*duration);
+}
+
+fn handle_stream(mut unix_stream: UnixStream) -> String {
+    let mut message = String::new();
+
+    unix_stream.read_to_string(&mut message).unwrap();
+
+    if message.starts_with("play") {
+        println!("playing...");
+    }
+
+    if message.starts_with("pause") {
+        println!("pausing...");
+    }
+    if message.starts_with("exit") {
+        println!("exiting...");
+    }
+    if message.starts_with("list") {
+        println!("listing...");
+    }
+    if message.starts_with("search") {
+        println!("searching...");
+    }
+    if message.starts_with("path") {
+        println!("chegou aqui legal");
+    }
+    if message.starts_with("next") {
+        println!("next...");
+    }
+    return message;
+}
